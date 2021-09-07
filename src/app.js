44a062dc9fed420cc9cf82a96beebce249b916b1
Mythily Rajendran <mythilyrajendran@Mythilys-MacBook-Air.local>
2021-09-07 13:22:32 +0530

    exporting and replacing writeJSON

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const dataJS= require('./data');
+const { accounts, users, writeJSON} = dataJS;
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
@@ -8,10 +10,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 const port = 3000;
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'),{encoding:'utf8', flag:'r'});
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'),{encoding:'utf8', flag:'r'});
-const users = JSON.parse(userData);
 app.get('/', (req, res) => {
     res.render('index',{ title: 'Account Summary',accounts:accounts, profile: '/profile', Profile: 'Karthik',transfer:'/transfer', Transfer:'Transfer' });
 });
@@ -33,8 +31,7 @@ app.get('/transfer', (req, res) => {
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON,'utf8');
+    writeJSON();
     res.render('transfer',{message: "Transfer Completed"})
 });
 app.get('/payment', (req, res) => {
@@ -43,8 +40,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
     accounts.credit.balance = accounts.credit.balance - req.body.amount;
     accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON,'utf8');
+    writeJSON();
     res.render('payment',{ message: "Payment Successful", account: accounts.credit });
 });
 app.listen(port, () => {
